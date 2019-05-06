bc269d50c751bc728185a03ca662685361b84201
Jason Harrel <harrel.jason@gmail.com>
2019-05-06 00:51:27 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
 
-define a method say_hello
-  def say_hello("Gabriela")
-    puts "Hello " + say_hello
-  end
+def say_hello(name="Ruby Programmer")
+  puts "Hello " + name + "!"
+end
+say_hello("Kent Beck")
